1293a3ccb86ac87ef18a41230c870027bf1f77ef
villageworks <villageworks@gmail.com>
2023-11-30 22:36:12 +0900

    ver.0.22: 商品一覧表示ロジック実装

--- src/main/java/com/example/demo/controller/ItemController.java
+++ src/main/java/com/example/demo/controller/ItemController.java
@@ -1,14 +1,28 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.entity.Item;
+
 @Controller
 public class ItemController {
+	
+	@Autowired
+	private ItemRepository itemRepository;
+	
 	// 商品一覧画面表示
 	@GetMapping("/items")
 	public String index(Model model) {
+		// 商品一覧を取得
+		List<Item> list = itemRepository.findAll();
+		// 取得した商品リストをスコープに設定
+		model.addAttribute("itemList", list);
+		// 画面遷移
 		return "itemsView";
 	}
 }
